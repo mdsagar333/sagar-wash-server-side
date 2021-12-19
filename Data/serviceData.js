ad27a378b025f1507780efeed64fda526c6abeff
mdsagar333 <md_sagar@yahoo.com>
2021-12-19 23:47:38 +0600

    first commit

---
+++ Data/serviceData.js
@@ -0,0 +1,32 @@
+const serviceData = [
+  {
+    type: "laundry",
+    servieTitle: "laundry service",
+    image: "https://i.ibb.co/NskJtdS/laundry.jpg",
+    perItem: 5,
+    text: "When you use our professional wash and fold services, you can be sure that your clothes will be well taken care of because we have served our customers in USA for over 80 years and we understand your needs.",
+  },
+  {
+    servieTitle: "dry cleaning service",
+    perItem: 8,
+    type: "dry cleaning",
+    image: "https://i.ibb.co/Qd0fX18/dry.jpg",
+    text: "Oil stains are stubborn and may not be entirely possible to remove by water.  Synthetic fibers like polyester respond well to only dry cleaning.",
+  },
+  {
+    type: "ironing",
+    servieTitle: "ironing service",
+    image: "https://i.ibb.co/QNjr0GY/iron.jpg",
+    perItem: 3,
+    text: "When you use our professional wash and fold services, you can be sure that your clothes will be well taken care of because we have served our customers in USA for over 80 years and we understand your needs.",
+  },
+  {
+    type: "alter & repairs",
+    servieTitle: "alter & repairs",
+    perItem: 10,
+    image: "https://i.ibb.co/cxjR3Ng/altering.jpg",
+    text: "Broken zip? Trousers too long? Lost weight or put some on? Don't worry; we can sort out all your clothing alterations and repairs",
+  },
+];
+
+module.exports = serviceData;
---
+++ index.js
@@ -0,0 +1,253 @@
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+var app = express();
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const { MongoClient, ObjectId } = require("mongodb");
+
+const port = process.env.PORT || 4000;
+
+const dbURL = `mongodb+srv://ayan:${process.env.DB_PASSWORD}@cluster0.tcz9h.mongodb.net/SagarWash?retryWrites=true&w=majority`;
+
+app.use(express.json());
+app.use(cors());
+
+const client = new MongoClient(dbURL);
+
+async function run() {
+  try {
+    await client
+      .connect()
+      .then(() => console.log("database connected"))
+      .catch((err) => console.log(err));
+
+    const database = client.db("SagarWash");
+    const Services = database.collection("service");
+    const Orders = database.collection("orders");
+    const Users = database.collection("users");
+
+    // All the API endpoints
+
+    app.post("/create-payment-intent", async (req, res) => {
+      try {
+        const { price } = req.body;
+        const amount = price * 100;
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: "usd",
+          payment_method_types: ["card"],
+        });
+
+        res.status(200).json({
+          status: "success",
+          clientSecret: paymentIntent.client_secret,
+        });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({
+          status: "fail",
+          error: err.message,
+        });
+      }
+    });
+
+    // create services
+    // app.post("/api/v1/services", async (req, res) => {
+    //   try {
+    //     const services = await Services.insertMany(serviceData);
+
+    //     console.log(services);
+    //     res.status(201).json({
+    //       status: "success",
+    //       services,
+    //     });
+    //   } catch (err) {
+    //     res.status(500).json({
+    //       status: "fail",
+    //       error: err.message,
+    //     });
+    //   }
+    // });
+
+    // get all services
+
+    app.get("/api/v1/services", async (req, res) => {
+      try {
+        const services = await Services.find({}).toArray();
+        res.status(201).json({
+          status: "success",
+          services,
+        });
+      } catch (err) {
+        res.status(500).json({
+          status: "fail",
+          error: err.message,
+        });
+      }
+    });
+
+    // get a service
+    app.get("/api/v1/services/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const newID = ObjectId(id);
+        const service = await Services.findOne({ _id: newID });
+        res.status(200).json({
+          status: "success",
+          service,
+        });
+      } catch (err) {
+        res.status(500).json({
+          status: "fail",
+          error: err.message,
+        });
+      }
+    });
+
+    // create user
+
+    app.patch("/api/v1/users", async (req, res) => {
+      try {
+        const user = req.body;
+        const createdUser = await Users.updateOne(
+          { uid: user.uid },
+          { $set: user },
+          {
+            upsert: true,
+          }
+        );
+        console.log(createdUser);
+        res.status(200).json({
+          status: "success",
+          createdUser,
+        });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({
+          status: "fail",
+          error: err.message,
+        });
+      }
+    });
+
+    app.get("/", (req, res) => {
+      res.send("Hello wrold");
+    });
+
+    // create order
+    app.post("/api/v1/orders", async (req, res) => {
+      try {
+        const order = await Orders.insertOne(req.body);
+        res.status(201).json({
+          status: "success",
+        });
+      } catch (err) {
+        res.status(500).json({
+          status: "fail",
+          error: err.message,
+        });
+      }
+    });
+
+    // get All orders
+    app.get("/api/v1/orders", async (req, res) => {
+      try {
+        const orders = await Orders.find({}).toArray();
+        res.status(200).json({
+          status: "success",
+          orders,
+        });
+      } catch (err) {
+        res.status(500).json({
+          status: "fail",
+          error: err.message,
+        });
+      }
+    });
+
+    // get orders by user
+    app.get("/api/v1/orders/:userID", async (req, res) => {
+      try {
+        const { userID } = req.params;
+        const orders = await Orders.find({ userUid: userID }).toArray();
+        res.status(200).json({
+          status: "success",
+          orders,
+        });
+      } catch (err) {
+        res.status(500).json({
+          status: "fail",
+          error: err.message,
+        });
+      }
+    });
+
+    // delete an order
+    app.delete("/api/v1/order/:orderId", async (req, res) => {
+      try {
+        const { orderId } = req.params;
+        const id = ObjectId(orderId);
+        const order = await Orders.deleteOne({ _id: id });
+
+        res.status(204).json({
+          status: "success",
+        });
+      } catch (err) {
+        res.status(500).json({
+          status: "fail",
+          error: err.message,
+        });
+      }
+    });
+
+    // get orders by user and id
+    app.get("/api/v1/order/:orderId", async (req, res) => {
+      try {
+        const { orderId } = req.params;
+        const id = ObjectId(orderId);
+        const order = await Orders.findOne({ _id: id });
+        res.status(200).json({
+          status: "success",
+          order,
+        });
+      } catch (err) {
+        res.status(500).json({
+          status: "fail",
+          error: err.message,
+        });
+      }
+    });
+
+    // update order
+    app.put("/api/v1/update-order/:orderId", async (req, res) => {
+      try {
+        const { orderId } = req.params;
+        const id = ObjectId(orderId);
+        console.log(id);
+        const update = await Orders.updateOne(
+          { _id: id },
+          { $set: { isPaid: true } }
+        );
+        console.log(update);
+        res.status(200).json({
+          status: "success",
+        });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({
+          status: "fail",
+          error: err.message,
+        });
+      }
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+// calling mongodb function
+run().catch(console.dir());
+
+app.listen(port, () => {
+  console.log("Your server listening on PORT", port);
+});
